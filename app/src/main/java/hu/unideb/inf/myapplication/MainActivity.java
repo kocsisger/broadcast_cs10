15ea3ac02e84768a20eaf0b49850f36540514723
Peter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2026-05-14 11:20:17 +0200

    pendingintent

--- app/src/main/java/hu/unideb/inf/myapplication/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/myapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package hu.unideb.inf.myapplication;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private BroadcastReceiver customReceiver = new CustomReceiver();
     static final String ACTION_CUSTOM_BROADCAST = "hu.unideb.inf.myapplication.MainActivity.ACTION_CUSTOM_BROADCAST_57edjmnoüöji9rtf68";
 
+    IntentFilter filter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +33,53 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        IntentFilter filter = new IntentFilter();
+        filter = new IntentFilter();
         filter.addAction(Intent.ACTION_POWER_CONNECTED);
         filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
         filter.addAction(ACTION_CUSTOM_BROADCAST);
 
+        new Thread(
+                () -> {
+                    for (int i = 0; i < 20; i++) {
+                        try {
+                            Thread.sleep(1000);
+                            Log.d("PendingIntent", "Count:" + (i+1));
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+        ).start();
+
+        Intent intent = new Intent(this, MyReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE
+                );
+
+        AlarmManager alarmManager = getSystemService(AlarmManager.class);
+        long triggerTime = System.currentTimeMillis() + 10*1000;
+
+        alarmManager.set(
+                AlarmManager.RTC_WAKEUP,
+                triggerTime,
+                pendingIntent
+        );
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
         this.registerReceiver(customReceiver, filter, RECEIVER_EXPORTED);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.unregisterReceiver(customReceiver);
     }
 
     public void sendCustomBroadcast(View view) {
